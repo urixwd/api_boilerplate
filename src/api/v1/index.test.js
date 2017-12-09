ddd3ac81979436fabed35df0abbe3a6eeececc75
Uri Michaeli <urixwd@gmail.com>
2017-12-09 16:32:10 +0100

    /user test

--- src/api/v1/index.test.js
+++ src/api/v1/index.test.js
@@ -15,4 +15,26 @@ describe(`API ${currentVersion} Tests`, () => {
       });
     });
   });
+  describe('# Create a new user', () => {
+    it('should not pass the validation (nothing sent)', (done) => {
+      request(global.app).post(`/api/${currentVersion}/user`).end((err, res) => {
+        expect(res.statusCode).to.equal(400);
+        done();
+      });
+    });
+    it('should not pass the validation (wrong SSN)', (done) => {
+      request(global.app).post(`/api/${currentVersion}/user`).send({ email: 'urixwd@gmail.com', ssn: '123-2222' }).end((err, res) => {
+        expect(res.statusCode).to.equal(400);
+        done();
+      });
+    });
+    it('should pass the validation', (done) => {
+      request(global.app).post(`/api/${currentVersion}/user`).send({ email: 'urixwd@gmail.com', ssn: '123-22-2222' }).end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body[0]).to.have.own.property('ssn');
+        expect(res.body[0]).to.have.own.property('email');
+        done();
+      });
+    });
+  });
 });
